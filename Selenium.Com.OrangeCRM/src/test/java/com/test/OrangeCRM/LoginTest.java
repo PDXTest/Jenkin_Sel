49a42a403eeb39ae66fc4bdd609570315808adfc
Lenovo <chandubhirud@yahoo.com>
2021-01-07 14:01:07 +0530

    New Code Added

--- Selenium.Com.OrangeCRM/src/test/java/com/test/OrangeCRM/LoginTest.java
+++ Selenium.Com.OrangeCRM/src/test/java/com/test/OrangeCRM/LoginTest.java
@@ -65,9 +65,9 @@ public class LoginTest extends TestBase{
 			test.log(LogStatus.PASS, "Before test");
 		       org.testng.Assert.assertEquals(dashaboard, "Dashboard");
 		       
-		       test.log(LogStatus.PASS, "Hello Chandu123");
+		       test.log(LogStatus.PASS, "Hello Chandu123hello");
 		       test.log(LogStatus.PASS, "Test is passed");
-		       test.log(LogStatus.PASS, "Hello Chandu123");
+		       test.log(LogStatus.PASS, "Hello Chandu123hello");
 		       
 		       
 		} catch (AssertionError e) {
